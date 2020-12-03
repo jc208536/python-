8ab661b3e07583e064e10a5f832a7931216b4f47
jc208536 <74966430+jc208536@users.noreply.github.com>
2020-12-03 17:17:52 +0800

    Update mail_02.py

--- mail_02.py
+++ mail_02.py
@@ -1,4 +1,4 @@
 from mail_.mail_send.sends_mail import send_mail_domo
 # from.mail_send import send_mail_domo
-send_mail_domo('1484387355@qq.com','jc_wyml@163.com','悠悠我心','青青子衿，悠悠我心，但为君故，沉吟至今')
+send_mail_domo('123456@qq.com','123456@163.com','悠悠我心','青青子衿，悠悠我心，但为君故，沉吟至今')
 
